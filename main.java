0e389c47182fd3964aaf297a41392c407147aa20
Francisco <86533117+Francisco-BM@users.noreply.github.com>
2022-01-12 16:59:08 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HOLa soy paco");
+        System.out.println("HOLA 2");
+        System.out.println("Arreglo2");
+    }
+}
